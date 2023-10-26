4d863b5ff8fa417c61827bd7ce57cd15194a7df4
Andrew Jose <arnav.jose@gmail.com>
2023-10-26 23:00:41 +0530

    bootstrap boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+mod nav;
+
+use leptos::*;
+use nav::Navbar;
+
 fn main() {
-    println!("Hello, world!");
+    mount_to_body(App)
 }
+
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = create_signal(0);
+
+    view! {
+        <Navbar/>
+        <button
+        on:click=move |_|{set_count.update(|n| *n += 1);}>
+        "Click me, Zozo: "
+        </button>
+        " "<strong class=("red", move || count()%2 == 0)>{count}</strong>
+        <br/>
+        <button on:click=move |_|(set_count(0))>
+        "Reset"
+        </button>
+    }
+}
+
